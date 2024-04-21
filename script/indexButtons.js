d2d5032c2e5d95ea73b36f8cb3cacfea6607b2c5
Chinkhusel14 <xrobertftw@gmail.com>
2024-04-21 20:10:24 +0800

    index popup start

---
+++ script/indexButtons.js
@@ -0,0 +1,7 @@
+const bairbtn = document.getElementById("bair-button");
+const bairlist = document.getElementById("bair-list");
+
+
+bairbtn.addEventListener("click", ()=>{
+   bairlist.classList.add("open");
+})
