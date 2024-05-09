8b3bf86a5853e422af167ddfdafed8d3044299a2
tuugu0730 <121286951+tuugu0730@users.noreply.github.com>
2024-05-10 04:34:26 +0800

    style edit

--- script/classPage.js
+++ script/classPage.js
@@ -17,12 +17,15 @@ addRatingBtn.addEventListener("click", () => {
   else ratingPopUP.classList.add("open");
 });
 
-// const timeBtn = document.getElementsByClassName("timeBtn");
-// timeBtn.addEventListener("click", () => {
-//   if (timeBtn.classList.contains("timeBtnClick"))
-//     timeBtn.classList.remove("timeBtnClick");
-//   else timeBtn.classList.add("timeBtnClick");
-// });
+let timeButtons = {};
+const timeBtns = document.getElementsByClassName("timeBtn");
+for (let i = 0; i < timeBtns.length; i++) {
+  timeBtns[i].addEventListener("click", () => {
+    if (timeBtns[i].classList.contains("timeBtnClick"))
+      timeBtns[i].classList.remove("timeBtnClick");
+    else timeBtns[i].classList.add("timeBtnClick");
+  });
+}
 
 const usp = new URLSearchParams(document.location.search);
 const classObj = JSON.parse(usp.get("id"));
@@ -32,26 +35,28 @@ let classTitleHTML = "";
 let mainInfoHTML = "";
 
 classTitleHTML += `
-    <h2 class="heading-3">
+    <h2 class="heading-3 title">
       <span class="room">${classObj.building} - ${classObj.roomNo}</span>
     <h2>
     <img src="styles/assets/class.jpg" alt="class image" class="class-img">
   `;
 
 mainInfoHTML += `
-    <li><span class="main-type">${classObj.type}</span></li>
-    <li><div class="vl"></div></li>
+    <li><span class="info-type">${classObj.type}</span></li>
+      <li><div class="vl"></div></li>
     <li>
         <div class="info-desc">Суудлын тоо</div>
-        <div class="seat-count-m heading-6">${classObj.capacity}</div>
+        <div class="info-attr heading-6 " id="seat-count-m">${
+          classObj.capacity
+        }</div>
     </li>
-    <li><div class="vl"></div></li>
+      <li><div class="vl"></div></li>
     <li>
-        <div class="info-desc" id="projector-info">Проектор</div>
+        <div class="info-desc">Проектор</div>
         ${
           classObj.projector === "Проектортой"
-            ? "<i class='fa-solid fa-check proj-state'></i>"
-            : "<i class='fa-solid fa-xmark proj-state'></i>"
+            ? "<i class='fa-solid fa-check info-attr' id='proj-state'></i>"
+            : "<i class='fa-solid fa-xmark info-attr' id='proj-state'></i>"
         }
     </li>
   `;
@@ -60,34 +65,34 @@ document.getElementById("main-title").innerHTML = classTitleHTML;
 document.getElementById("main-desc").innerHTML = mainInfoHTML;
 
 // fetch similar classes
-// fetch("https://api.npoint.io/70107af397f4a981c076")
-//   .then((response) => response.json())
-//   .then((responseObj) => {
-//     let tempDepart, tempType;
-//     if (classObj.building == "E-lib") tempDepart = "Е-Номын сан";
-//     else if (classObj.building == "1") tempDepart = "Хичээлийн төв байр";
-//     else if (classObj.building == "Хууль")
-//       tempDepart = "Улаанбаатар сургуулийн хичээлийн байр";
-//     else tempDepart = "Хичээлийн байр " + classObj.building;
+fetch("https://api.npoint.io/70107af397f4a981c076")
+  .then((response) => response.json())
+  .then((responseObj) => {
+    let tempDepart, tempType;
+    if (classObj.building == "E-lib") tempDepart = "Е-Номын сан";
+    else if (classObj.building == "1") tempDepart = "Хичээлийн төв байр";
+    else if (classObj.building == "Хууль")
+      tempDepart = "Улаанбаатар сургуулийн хичээлийн байр";
+    else tempDepart = "Хичээлийн байр " + classObj.building;
 
-//     if (classObj.type == "Семинар") tempType = "Семинарын танхим";
-//     else if (classObj.type == "Лекц") tempType = "Лекцийн танхим";
-//     else if (classObj.type == "Лаб") tempType = "Сургалтын лаборатори";
+    if (classObj.type == "Семинар") tempType = "Семинарын танхим";
+    else if (classObj.type == "Лекц") tempType = "Лекцийн танхим";
+    else if (classObj.type == "Лаб") tempType = "Сургалтын лаборатори";
 
-//     let filteredClasses = responseObj.filter(
-//       (similarClass) =>
-//         similarClass.Хичээлийн_байр == tempDepart &&
-//         similarClass.Өрөөний_зориулалт == tempType &&
-//         similarClass.Хичээлийн_хуваарь_тавих_боломж != "Хуваарь тавих боломжгүй"
-//     );
+    let filteredClasses = responseObj.filter(
+      (similarClass) =>
+        similarClass.Хичээлийн_байр == tempDepart &&
+        similarClass.Өрөөний_зориулалт == tempType &&
+        similarClass.Хичээлийн_хуваарь_тавих_боломж != "Хуваарь тавих боломжгүй"
+    );
 
-//     const simClassHTMLArray = filteredClasses.map((simClassObj) => {
-//       const classI = new ClassSec(simClassObj);
-//       return classI.Render();
-//     });
+    const simClassHTMLArray = filteredClasses.map((simClassObj) => {
+      const classI = new ClassSec(simClassObj, "");
+      return classI.Render();
+    });
 
-//     const simClassHTML = simClassHTMLArray.reduce(
-//       (prev, current) => prev + current
-//     );
-//     document.getElementById("s-class-list").innerHTML = simClassHTML;
-//   });
+    const simClassHTML = simClassHTMLArray.reduce(
+      (prev, current) => prev + current
+    );
+    document.getElementById("s-class-list").innerHTML = simClassHTML;
+  });
