6c8cf7d144046f6b6295dccef844efcf9a55d51b
tuugu0730 <121286951+tuugu0730@users.noreply.github.com>
2024-05-22 12:53:01 +0800

    db

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,4 +1,11 @@
 import { createServer } from "node:http";
+import pool from "./db.mjs";
+import express from "express";
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json()); // req.body
 
 const hostname = "127.0.0.1";
 const port = 3000;
