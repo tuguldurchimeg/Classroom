957c1a8645e0577e0d2849f60f63aaa1c7af9135
tuugu0730 <121286951+tuugu0730@users.noreply.github.com>
2024-05-25 14:41:27 +0800

    update

---
+++ components/Stars.js
@@ -0,0 +1,34 @@
+class Stars extends HTMLElement {
+  constructor() {
+    super();
+    this.totalStars = 5;
+    this.type = this.getAttribute("tp");
+    this.rating = 0;
+  }
+  #Render() {
+    const stars = this.setStarsMap();
+    return `<span style="color: var(--color-text)">${this.rating}</span>
+            ${stars}`;
+  }
+  connectedCallback() {
+    this.innerHTML = this.#Render();
+  }
+  setRating = (rate) => {
+    this.rating = rate;
+    this.innerHTML = this.#Render();
+  };
+  setStarsMap = () => {
+    return [...Array(this.totalStars)]
+      .map((star, index) => {
+        const currentRating = index + 1;
+        return `<star-rating color = ${
+          currentRating <= this.rating
+            ? "var(--color-primary)"
+            : "var(--color-border)"
+        }" tp=${this.type} val=${currentRating}></star-rating>`;
+      })
+      .reduce((prev, current) => prev + current);
+  };
+}
+
+window.customElements.define("stars-rating", Stars);
